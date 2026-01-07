1efaf6c11fa1e10aac009f3a6c3dd9e731cb48b3
Antonio Šulenta <as55007@fer.hr>
2026-01-07 17:23:05 +0100

    New Interpolation Values

--- audio-visualization-test/SignalProcessing.swift
+++ audio-visualization-test/SignalProcessing.swift
@@ -23,17 +23,37 @@ class SignalProcessing {
     }
     
     static func interpolate(current: Float, previous: Float) -> [Float] {
-        var vals = [Float](repeating: 0, count: 11)
-        vals[10] = current
-        vals[5] = (current + previous)/2
-        vals[2] = (vals[5] + previous)/2
+        var vals = [Float](repeating: 0, count: 31)
+        vals[30] = current
+        vals[15] = (current + previous)/2
+        vals[8] = (vals[15] + previous)/2
+        vals[4] = (vals[8] + previous)/2
+        vals[2] = (vals[4] + previous)/2
         vals[1] = (vals[2] + previous)/2
-        vals[8] = (vals[5] + current)/2
-        vals[9] = (vals[10] + current)/2
-        vals[7] = (vals[5] + vals[9])/2
-        vals[6] = (vals[5] + vals[7])/2
-        vals[3] = (vals[1] + vals[5])/2
-        vals[4] = (vals[3] + vals[5])/2
+        vals[28] = (vals[15] + current)/2
+        vals[14] = (vals[8] + vals[28])/2
+        vals[7] = (vals[4] + vals[14])/2
+        vals[23] = (vals[14] + vals[28])/2
+        vals[11] = (vals[8] + vals[14])/2
+        vals[5] = (vals[2] + vals[8])/2
+        vals[3] = (vals[1] + vals[5])
+        vals[29] = (vals[28] + current)/2
+        vals[26] = (vals[23] + vals[29])/2
+        vals[22] = (vals[14] + current)/2
+        vals[20] = (vals[29] + vals[11])/2
+        vals[18] = (vals[14] + vals[22])/2
+        vals[16] = (vals[14] + vals[18])/2
+        vals[13] = (vals[11] + vals[15])/2
+        vals[12] = (vals[8] + vals[16])/2
+        vals[10] = (vals[7] + vals[13])/2
+        vals[9] = (vals[5] + vals[13])/2
+        vals[6] = (vals[3] + vals[9])/2
+        vals[24] = (vals[22] + vals[26])/2
+        vals[27] = (vals[24] + current)/2
+        vals[26] = (vals[29] + vals[24])/2
+        vals[21] = (vals[24] + vals[18])/2
+        vals[17] = (vals[20] + vals[14])/2
+        vals[19] = (vals[17] + vals[21])/2
         vals[0] = (previous + vals[1])/2
 
         return vals
